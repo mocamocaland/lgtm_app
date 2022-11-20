13cf15810794b0353bc5781470365aa015750dbb
mocamocaland <noi_kaas@yahoo.co.jp>
2022-11-20 19:23:56 +0900

    add setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import  find_packages, setup
+from setuptools import find_packages, setup
 
 setup(
     name='lgtm',
     version='1.0.0',
     packages=find_packages(exclude=('tests',)),
     install_requires=[
-        'Click',
-        'Pillow',
-        'requests',
+        'Click~=8.0',
+        'Pillow~=9.3.0',
+        'requests~=2.28.0',
     ],
     entry_points={
         'console_scripts': [
