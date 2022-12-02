0ba1d0696232388937e61f164477c7ba930d7fe3
mocamocaland <noi_kaas@yahoo.co.jp>
2022-12-02 22:34:23 +0900

    mofiy python-app.yml add pytest at test_core.py, requirements.txt

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,14 @@
-import unittest
+# import unittest
+#
+#
+# class LgtmTest(unittest.TestCase):
+#     def test_lgtm(self):
+#         from lgtm.core import lgtm
+#         self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))  # add assertion here
+
+import pytest
 
 
-class LgtmTest(unittest.TestCase):
-    def test_lgtm(self):
-        from lgtm.core import lgtm
-        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))  # add assertion here
+def test_lgtm():
+    from lgtm.core import lgtm
+    assert (lgtm('./python.jpeg', 'LGTM')) is None  # add assertion here
