a740e1d40a0a497de432f6f19d245da0c2dcebad
mocamocaland <noi_kaas@yahoo.co.jp>
2022-12-03 19:27:15 +0900

    mofiy

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,11 +1,11 @@
-# import unittest
-#
-#
-# class LgtmTest(unittest.TestCase):
-#     def test_lgtm(self):
-#         from lgtm.core import lgtm
-#         self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))  # add assertion here
+import unittest
 
-def test_lgtm():
-    from lgtm.core import lgtm
-    assert (lgtm('./python.jpeg', 'LGTM')) is None  # add assertion here
+
+class LgtmTest(unittest.TestCase):
+    def test_lgtm(self):
+        from lgtm.core import lgtm
+        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))  # add assertion here
+
+# def test_lgtm():
+#     from lgtm.core import lgtm
+#     assert (lgtm('./python.jpeg', 'LGTM')) is None  # add assertion here
